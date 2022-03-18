268ece5f9d81cedb19b385dee3d5d3d7d2da4187
imronpuji <imronpuji5@gmail.com>
2022-03-18 19:22:39 +0700

    pu

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-	res.json('hello guys')
+	res.json('hello world')
 })
 const port = 5000
 app.listen(port, () => console.log('running'))
